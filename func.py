9868e3c173a1bdc76288d594bb16dca3b69683c8
Chrystal Chavez <cchav083@ucr.edu>
2024-10-28 23:03:19 -0700

    Added prompts and populate function

--- func.py
+++ func.py
@@ -52,6 +52,17 @@ def get_states(state):
         if currState is not None:
             newStates.append(currState)
     return newStates
+
+def populate():
+    print("Enter your 3 by 3 puzzle, type in \"0\" to represent the blank")
+    row1 = input("Enter the first row, use a space between each number: ")
+    row2 = input("Enter the second row, use a space between each number: ")
+    row3 = input("Enter the third row, use a space between each number: ")
+    intRow1 = [int(s) for s in re.findall(r'\d+', row1)] 
+    intRow2 = [int(s) for s in re.findall(r'\d+', row2)] 
+    intRow3 = [int(s) for s in re.findall(r'\d+', row3)] 
+    initialState = np.array([intRow1, intRow2, intRow3])
+    return initialState
             
 
 def search(goalState,state):
--- main.py
+++ main.py
@@ -5,4 +5,31 @@ import numpy as np
 initState = np.array([[1,2,3],[4,8,0],[7,6,5]])
 goalState = np.array([[1,2,3],[4,5,6],[7,8,0]])
 
-
+while(1):
+    print("Welcome to CS170 8 puzzle solver.")
+    userInput = input("Type \"1\" to use a default puzzle, or \"2\" to enter your own puzzle: ") 
+    if (userInput == "1"):
+        puzzle = classes.Problem()
+        break
+    elif (userInput == "2"):
+        puzzle = classes.Problem(func.populate())
+        break
+    else:
+        print("Invalid choice. Please choose from the options prompted")
+while(1):
+    print("Enter your choice of algorithm")
+    print("Type \"1\" for Uniform Cost Search.")
+    print("Type \"2\" for A* with the Misplaced Tile heuristic.")
+    print("Type \"3\" for A* with the Euclidean distance heuristic.")
+    userInput = input()
+    if(userInput == "1"):
+        # Uniform Cost Search
+        break
+    elif(userInput == "2"):
+        # A* with the Misplaced Tile heuristic
+        break
+    elif(userInput == "3"):
+        # A* with the Euclidean distance heuristic
+        break
+    else:
+        print("Invalid choice. Please choose from the options prompted")
